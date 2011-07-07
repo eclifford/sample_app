279100717fc85064c584ab96e4419bd91bd63765
eclifford <ericgclifford@gmail.com>
2011-07-07 15:45:46 -0700

    Added stylesheets

---
+++ example_user.rb
@@ -0,0 +1,16 @@
+class User
+  # Define the getters/setters known as attribute accessors in ROR
+  attr_accessor :name, :email
+  
+  # Construtor method called when we call User.new
+  # We are passing an empty hash by default
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
+  
+  # 
+  def formatted_email
+    "#{@name} <#{@email}>" # Eric Clifford <ericgclifford@gmail.com>
+  end
+end
