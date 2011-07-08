357daead2db1ce523992b3c72cf9f4bcdcf40712
eclifford <ericgclifford@gmail.com>
2011-07-08 15:01:20 -0700

    Finished first draft of User Model

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+# Schema version: 20110708192915
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+  
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :uniqueness => { :case_sensitive => false }
+end
